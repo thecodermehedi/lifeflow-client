9f947962a443d308f9f51ec405f8924387ff2cbb
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-25 01:48:26 +0600

    Add main.jsx file with initial code

---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import ReactDOM from "react-dom/client";
+import {HelmetProvider} from "react-helmet-async";
+import AuthProvider from "./providers/AuthProvider";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
+import {Toaster} from "react-hot-toast";
+import {RouterProvider} from "react-router-dom";
+import {router} from "./routes";
+import "./index.css";
+
+const queryClient = new QueryClient();
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HelmetProvider>
+    <AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <Toaster />
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </AuthProvider>
+  </HelmetProvider>
+);
