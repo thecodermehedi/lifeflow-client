d7f5d67d5fb055f21bd05f62463a69341833d2ec
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-26 03:00:06 +0600

    Add API utility functions for districts, upazilas, and photo upload

---
+++ src/api/utils.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import axiosPublic from "./axiosPublic";
+
+export const getDistricts = async () => {
+  const {data} = await axiosPublic("/districts");
+  return data;
+};
+
+export const getUpazilas = async () => {
+  const {data} = await axiosPublic("/upazilas");
+  return data;
+};
+
+export const uploadPhoto = async (photo) => {
+  const formData = new FormData();
+  formData.append("image", photo);
+  // console.log("photo", photo);
+  const {data} = await axios.post(
+    `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_API_KEY}`,
+    formData
+  );
+  return data;
+};
