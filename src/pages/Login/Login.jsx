a36574a61e65b281e2f17ab750fe22af889bbc53
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-26 02:58:54 +0600

    Add login form with password toggle

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,7 +1,158 @@
+import {useState} from "react";
+import Container from "../../components/Shared/Container";
+import {Link} from "react-router-dom";
+
 const Login = () => {
+  const [isPasswordHidden, setPasswordHidden] = useState(true);
   return (
-    <section className="text-center w-full min-h-screen text-xl">
-      <h1>Login</h1>
+    <section className="min-h-screen bg-gray-100">
+      <Container className={"flex justify-center items-center h-screen "}>
+        <div className=" max-w-lg rounded-2xl p-5 md:p-8 lg:p-10 mt-18 bg-white shadow-xl">
+          <h1 className="text-center md:text-2xl lg:text-3xl font-bold text-primary text-xl">
+            Donate Blood, Save Lives
+          </h1>
+          <p className="mx-auto mt-4 max-w-md text-sm md:text-lg text-center text-gray-500">
+            Join our community of lifesavers. Your donation can make a
+            difference.
+          </p>
+          <form
+            action=""
+            className="mb-0 mt-6 space-y-4 rounded-lg p-4 sm:p-6 lg:p-8"
+          >
+            <p className="text-center text-lg font-medium">
+              Log in to your account
+            </p>
+            <div>
+              <div className="relative">
+                <svg
+                  className="w-6 h-6 text-gray-400 absolute left-3 inset-y-0 my-auto"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21.75 6.75v10.5a2.25 2.25 0 01-2.25 2.25h-15a2.25 2.25 0 01-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0019.5 4.5h-15a2.25 2.25 0 00-2.25 2.25m19.5 0v.243a2.25 2.25 0 01-1.07 1.916l-7.5 4.615a2.25 2.25 0 01-2.36 0L3.32 8.91a2.25 2.25 0 01-1.07-1.916V6.75"
+                  />
+                </svg>
+                <input
+                  type="text"
+                  placeholder="Email address"
+                  className="w-full pl-12 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-primary shadow-sm rounded-lg"
+                />
+              </div>
+            </div>
+            <div className="relative w-full mt-2">
+              <button
+                type="button"
+                className="text-gray-400 absolute right-3 inset-y-0 my-auto active:text-gray-600"
+                onClick={() => setPasswordHidden(!isPasswordHidden)}
+              >
+                {isPasswordHidden ? (
+                  <svg
+                    className="w-6 h-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3.98 8.223A10.477 10.477 0 001.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.45 10.45 0 0112 4.5c4.756 0 8.773 3.162 10.065 7.498a10.523 10.523 0 01-4.293 5.774M6.228 6.228L3 3m3.228 3.228l3.65 3.65m7.894 7.894L21 21m-3.228-3.228l-3.65-3.65m0 0a3 3 0 10-4.243-4.243m4.242 4.242L9.88 9.88"
+                    />
+                  </svg>
+                )}
+              </button>
+              <input
+                type={isPasswordHidden ? "password" : "text"}
+                placeholder="Enter Password"
+                className="w-full pr-12 pl-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-primary shadow-sm rounded-lg"
+              />
+            </div>
+
+            <button
+              type="submit"
+              className="block w-full  px-5 py-3 te font-medium btn"
+            >
+              <span> Sign in</span>
+            </button>
+            <div className="flex items-center justify-between">
+              <span className="w-1/5 border-b lg:w-1/4"></span>
+              <p className="text-xs text-center text-gray-500 uppercase">or</p>
+              <span className="w-1/5 border-b lg:w-1/4"></span>
+            </div>
+            <button
+              type="button"
+              className="w-full py-3 border font-medium btn-hover"
+            >
+              <span className="flex items-center justify-center gap-x-3 text-black">
+                <svg
+                  className="w-5 h-5"
+                  viewBox="0 0 48 48"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g clipPath="url(#clip0_17_40)">
+                    <path
+                      d="M47.532 24.5528C47.532 22.9214 47.3997 21.2811 47.1175 19.6761H24.48V28.9181H37.4434C36.9055 31.8988 35.177 34.5356 32.6461 36.2111V42.2078H40.3801C44.9217 38.0278 47.532 31.8547 47.532 24.5528Z"
+                      fill="#4285F4"
+                    />
+                    <path
+                      d="M24.48 48.0016C30.9529 48.0016 36.4116 45.8764 40.3888 42.2078L32.6549 36.2111C30.5031 37.675 27.7252 38.5039 24.4888 38.5039C18.2275 38.5039 12.9187 34.2798 11.0139 28.6006H3.03296V34.7825C7.10718 42.8868 15.4056 48.0016 24.48 48.0016Z"
+                      fill="#34A853"
+                    />
+                    <path
+                      d="M11.0051 28.6006C9.99973 25.6199 9.99973 22.3922 11.0051 19.4115V13.2296H3.03298C-0.371021 20.0112 -0.371021 28.0009 3.03298 34.7825L11.0051 28.6006Z"
+                      fill="#FBBC04"
+                    />
+                    <path
+                      d="M24.48 9.49932C27.9016 9.44641 31.2086 10.7339 33.6866 13.0973L40.5387 6.24523C36.2 2.17101 30.4414 -0.068932 24.48 0.00161733C15.4055 0.00161733 7.10718 5.11644 3.03296 13.2296L11.005 19.4115C12.901 13.7235 18.2187 9.49932 24.48 9.49932Z"
+                      fill="#EA4335"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_17_40">
+                      <rect width="48" height="48" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
+                Continue with Google
+              </span>
+            </button>
+            <p className="text-center text-sm text-gray-500">
+              Don&#39;t have an account?{" "}
+              <Link className="underline text-primary" to={"/register"}>
+                Register
+              </Link>
+            </p>
+          </form>
+        </div>
+      </Container>
     </section>
   );
 };
