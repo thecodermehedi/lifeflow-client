6f7ea2428dcf263f5557078c08169f5d08207411
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-28 21:28:35 +0600

    Refactor login page form and add form validation

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,13 +1,44 @@
 import {useState} from "react";
 import Container from "../../components/Shared/Container";
 import {Link} from "react-router-dom";
-
+import {useForm} from "react-hook-form";
+import useAuth from "../../hooks/useAuth";
+import toast from "react-hot-toast";
+import {useNavigate} from "react-router-dom";
 const Login = () => {
+  // Navigation
+  const navigate = useNavigate();
+
+  // Auth functions
+  const {signIn} = useAuth();
+
+  // Form handling
+  const {register, handleSubmit} = useForm();
+
+  // Password visibility state
   const [isPasswordHidden, setPasswordHidden] = useState(true);
+
+  // Form submit handler
+  const handleSignIn = async (data) => {
+    const toastLoading = toast.loading("Please wait...");
+    try {
+      await signIn(data?.email, data?.password);
+      navigate("/");
+      toast.success("Logged in successfully", {id: toastLoading});
+    } catch (error) {
+      console.log(error);
+      toast.error(
+        error.message === "Firebase: Error (auth/invalid-login-credentials)."
+          ? "Incorrect email or password"
+          : error.message,
+        {id: toastLoading}
+      );
+    }
+  };
   return (
     <section className="min-h-screen bg-gray-100">
       <Container className={"flex justify-center items-center h-screen "}>
-        <div className=" max-w-lg rounded-2xl p-5 md:p-8 lg:p-10 mt-18 bg-white shadow-xl">
+        <div className=" max-w-lg rounded-2xl p-5 md:p-8 lg:p-10 mt-18 ">
           <h1 className="text-center md:text-2xl lg:text-3xl font-bold text-primary text-xl">
             Donate Blood, Save Lives
           </h1>
@@ -16,12 +47,12 @@ const Login = () => {
             difference.
           </p>
           <form
-            action=""
-            className="mb-0 mt-6 space-y-4 rounded-lg p-4 sm:p-6 lg:p-8"
+            onSubmit={handleSubmit(handleSignIn)}
+            className="mb-0 mt-6 space-y-4 rounded-lg p-4 sm:p-6 lg:p-8 bg-white shadow-xl"
           >
-            <p className="text-center text-lg font-medium">
+            <h3 className="text-gray-800 text-xl font-bold md:text-2xl pb-5 text-center">
               Log in to your account
-            </p>
+            </h3>
             <div>
               <div className="relative">
                 <svg
@@ -39,9 +70,13 @@ const Login = () => {
                   />
                 </svg>
                 <input
-                  type="text"
-                  placeholder="Email address"
+                  type="email"
+                  id="email"
+                  name="email"
+                  {...register("email", {required: true})}
+                  placeholder="Email Address"
                   className="w-full pl-12 pr-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-primary shadow-sm rounded-lg"
+                  required
                 />
               </div>
             </div>
@@ -90,8 +125,12 @@ const Login = () => {
               </button>
               <input
                 type={isPasswordHidden ? "password" : "text"}
-                placeholder="Enter Password"
+                id="password"
+                name="password"
+                {...register("password", {required: true})}
+                placeholder="Enter your password"
                 className="w-full pr-12 pl-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-primary shadow-sm rounded-lg"
+                required
               />
             </div>
 
@@ -100,49 +139,6 @@ const Login = () => {
               className="block w-full  px-5 py-3 te font-medium btn"
             >
               <span> Sign in</span>
-            </button>
-            <div className="flex items-center justify-between">
-              <span className="w-1/5 border-b lg:w-1/4"></span>
-              <p className="text-xs text-center text-gray-500 uppercase">or</p>
-              <span className="w-1/5 border-b lg:w-1/4"></span>
-            </div>
-            <button
-              type="button"
-              className="w-full py-3 border font-medium btn-hover"
-            >
-              <span className="flex items-center justify-center gap-x-3 text-black">
-                <svg
-                  className="w-5 h-5"
-                  viewBox="0 0 48 48"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <g clipPath="url(#clip0_17_40)">
-                    <path
-                      d="M47.532 24.5528C47.532 22.9214 47.3997 21.2811 47.1175 19.6761H24.48V28.9181H37.4434C36.9055 31.8988 35.177 34.5356 32.6461 36.2111V42.2078H40.3801C44.9217 38.0278 47.532 31.8547 47.532 24.5528Z"
-                      fill="#4285F4"
-                    />
-                    <path
-                      d="M24.48 48.0016C30.9529 48.0016 36.4116 45.8764 40.3888 42.2078L32.6549 36.2111C30.5031 37.675 27.7252 38.5039 24.4888 38.5039C18.2275 38.5039 12.9187 34.2798 11.0139 28.6006H3.03296V34.7825C7.10718 42.8868 15.4056 48.0016 24.48 48.0016Z"
-                      fill="#34A853"
-                    />
-                    <path
-                      d="M11.0051 28.6006C9.99973 25.6199 9.99973 22.3922 11.0051 19.4115V13.2296H3.03298C-0.371021 20.0112 -0.371021 28.0009 3.03298 34.7825L11.0051 28.6006Z"
-                      fill="#FBBC04"
-                    />
-                    <path
-                      d="M24.48 9.49932C27.9016 9.44641 31.2086 10.7339 33.6866 13.0973L40.5387 6.24523C36.2 2.17101 30.4414 -0.068932 24.48 0.00161733C15.4055 0.00161733 7.10718 5.11644 3.03296 13.2296L11.005 19.4115C12.901 13.7235 18.2187 9.49932 24.48 9.49932Z"
-                      fill="#EA4335"
-                    />
-                  </g>
-                  <defs>
-                    <clipPath id="clip0_17_40">
-                      <rect width="48" height="48" fill="white" />
-                    </clipPath>
-                  </defs>
-                </svg>
-                Continue with Google
-              </span>
             </button>
             <p className="text-center text-sm text-gray-500">
               Don&#39;t have an account?{" "}
