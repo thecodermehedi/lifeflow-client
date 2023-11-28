7ab4ece8c535287902b76919ef6b974d1e35055d
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-29 00:24:43 +0600

    Add new routes and update existing routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -18,6 +18,11 @@ import Content from "../pages/Dashboard/Common/Content/Content";
 import AddBlog from "../pages/Dashboard/Admin/AddBlog/AddBlog";
 import UpdateRequest from "../pages/Dashboard/UpdateRequest/UpdateRequest";
 import DonationDetails from "../pages/Dashboard/Common/DonationDetails/DonationDetails";
+import BlockRoute from "./BlockRoute";
+import AdminRoute from "./AdminRoute";
+import VolunteerRoute from "./VolunteerRoute";
+import PrivateRoute from "./PrivateRoute";
+import Spinner from "../components/Spinner";
 
 const router = createBrowserRouter([
   {
@@ -31,25 +36,114 @@ const router = createBrowserRouter([
       {path: "/donation-request", element: <DonationRequest />},
     ],
   },
-  {path: "/login", element: <Login />},
-  {path: "/register", element: <Register />},
+  {
+    path: "/login",
+    element: (
+      <BlockRoute>
+        <Login />
+      </BlockRoute>
+    ),
+  },
+  {
+    path: "/register",
+    element: (
+      <BlockRoute>
+        <Register />
+      </BlockRoute>
+    ),
+  },
+  {path: "/loading", element: <Spinner />},
   {
     path: "/dashboard",
-    element: <DashboardLayout />,
+    element: (
+      <PrivateRoute>
+        <DashboardLayout />
+      </PrivateRoute>
+    ),
     children: [
-      {path: "home", element: <DashHome />},
-      {path: "profile", element: <Profile />},
-      {path: "request/:id/edit", element: <UpdateRequest />},
-      {path: "request/:id/details", element: <DonationDetails />},
+      {
+        path: "home",
+        element: (
+          <PrivateRoute>
+            <DashHome />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "profile",
+        element: (
+          <PrivateRoute>
+            <Profile />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "request/:id/edit",
+        element: (
+          <PrivateRoute>
+            <UpdateRequest />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "request/:id/details",
+        element: (
+          <PrivateRoute>
+            <DonationDetails />
+          </PrivateRoute>
+        ),
+      },
 
       // Donor
-      {path: "my-donation-requests", element: <MyRequest />},
-      {path: "create-donation-request", element: <CreateRequest />},
+      {
+        path: "my-donation-requests",
+        element: (
+          <PrivateRoute>
+            <MyRequest />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "create-donation-request",
+        element: (
+          <PrivateRoute>
+            <CreateRequest />
+          </PrivateRoute>
+        ),
+      },
       // Admin
-      {path: "all-users", element: <Users />},
-      {path: "all-blood-donation-request", element: <Requests />},
-      {path: "content-management", element: <Content />},
-      {path: "content-management/add-blog", element: <AddBlog />},
+      {
+        path: "all-users",
+        element: (
+          <PrivateRoute>
+            <Users />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "all-blood-donation-request",
+        element: (
+          <PrivateRoute>
+            <Requests />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "content-management",
+        element: (
+          <PrivateRoute>
+            <Content />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "content-management/add-blog",
+        element: (
+          <PrivateRoute>
+            <AddBlog />
+          </PrivateRoute>
+        ),
+      },
       // Volunteer
       // {path: "my-donation-requests", element: <Dashboard />},
       // {path: "content-management", element: <Dashboard />},
