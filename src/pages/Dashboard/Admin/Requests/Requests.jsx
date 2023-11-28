0dba7b3caa1f7e5af126da895b60784a0242ff78
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-28 21:26:54 +0600

    Add Requests component to Admin Dashboard

---
+++ src/pages/Dashboard/Admin/Requests/Requests.jsx
@@ -0,0 +1,12 @@
+
+
+const Requests = () => {
+  return (
+    <section className='text-center w-full min-h-screen text-xl'>
+      <h1>Requests</h1>
+      
+    </section>
+  );
+};
+
+export default Requests;
