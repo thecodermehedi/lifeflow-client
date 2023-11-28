806a9ad8c1cd19d7ee3fd0d2d01cad60237aea75
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-28 21:26:49 +0600

    Add AddBlog component

---
+++ src/pages/Dashboard/Admin/AddBlog/AddBlog.jsx
@@ -0,0 +1,12 @@
+
+
+const AddBlog = () => {
+  return (
+    <section className='text-center w-full min-h-screen text-xl'>
+      <h1>AddBlog</h1>
+      
+    </section>
+  );
+};
+
+export default AddBlog;
