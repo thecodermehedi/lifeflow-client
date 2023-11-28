31513a4bbac8637bdc2a5fe15b1d60fb29a5f5b2
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-28 21:27:06 +0600

    Add Content component to Dashboard

---
+++ src/pages/Dashboard/Common/Content/Content.jsx
@@ -0,0 +1,12 @@
+
+
+const Content = () => {
+  return (
+    <section className='text-center w-full min-h-screen text-xl'>
+      <h1>Content</h1>
+      
+    </section>
+  );
+};
+
+export default Content;
