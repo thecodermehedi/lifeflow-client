cff1166a7f82f44922add3de99c50fa359c27b3b
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-28 21:26:59 +0600

    Add Users component to Dashboard/Admin/Users directory

---
+++ src/pages/Dashboard/Admin/Users/Users.jsx
@@ -0,0 +1,12 @@
+
+
+const Users = () => {
+  return (
+    <section className='text-center w-full min-h-screen text-xl'>
+      <h1>Users</h1>
+      
+    </section>
+  );
+};
+
+export default Users;
