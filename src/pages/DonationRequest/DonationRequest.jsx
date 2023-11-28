bf7f932ef558cb50b53bb5f5afddff225b748de3
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-28 21:28:08 +0600

    Add DonationRequest component

---
+++ src/pages/DonationRequest/DonationRequest.jsx
@@ -0,0 +1,9 @@
+const DonationRequest = () => {
+  return (
+    <section className="text-center w-full min-h-screen text-xl">
+      <h1>DonationRequest</h1>
+    </section>
+  );
+};
+
+export default DonationRequest;
