13112853ea4dde6514461cc0b2e496949e2deecd
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-29 21:34:03 +0600

    Remove console.log statement in uploadPhoto function

--- src/api/utils.js
+++ src/api/utils.js
@@ -14,7 +14,6 @@ export const getUpazilas = async () => {
 export const uploadPhoto = async (photo) => {
   const formData = new FormData();
   formData.append("image", photo);
-  // console.log("photo", photo);
   const {data} = await axios.post(
     `https://api.imgbb.com/1/upload?key=${import.meta.env.VITE_IMGBB_API_KEY}`,
     formData
