296991c01b70066d62b7f9fc414ac2abe9aeb94b
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-30 10:23:14 +0600

    Add CRUD operations for blogs

--- src/api/blog.js
+++ src/api/blog.js
@@ -1,6 +1,29 @@
 import axiosPublic from "./axiosPublic";
+import axiosSecure from ".";
 
 export const getBlogs = async () => {
   const {data} = await axiosPublic("/blogs");
   return data;
 };
+
+export const addBlog = async (blog) => {
+  const {data} = await axiosSecure.post("/blogs", blog);
+  return data;
+};
+
+export const deleteBlog = async (id) => {
+  const {data} = await axiosSecure.delete(`/blog/${id}`);
+  return data;
+}
+
+export const updateBlogStatus = async (id, status) => {
+  const {data} = await axiosSecure.patch(`/blog/status/${id}`, {status});
+  return data;
+}
+
+export const updateBlog = async (id, newBlogInfo) => {
+
+  console.log(id, newBlogInfo);
+  const {data} = await axiosSecure.patch(`/blog/${id}`, newBlogInfo);
+  return data;
+}
