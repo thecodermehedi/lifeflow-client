eb95e0a7ed583620e88a85141d46b14580a282a9
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-30 10:23:23 +0600

    Add useMutation hooks and update useBlog function

--- src/hooks/useBlog.jsx
+++ src/hooks/useBlog.jsx
@@ -1,8 +1,10 @@
-import {useQuery} from "@tanstack/react-query";
+import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
 import useUser from "./useUser";
 import useAuth from "./useAuth";
-import {getBlogs} from "../api/blog";
+import {addBlog, deleteBlog, getBlogs, updateBlog, updateBlogStatus} from "../api/blog";
+import toast from "react-hot-toast";
 const useBlog = () => {
+  const queryClient = useQueryClient();
   const {user, isUserLoading} = useAuth();
   const userMail = user?.email;
   const {currentUser} = useUser();
@@ -14,13 +16,76 @@ const useBlog = () => {
     queryFn: async () => await getBlogs(),
   });
 
+  const {mutateAsync: addBlogFn} = useMutation({
+    mutationFn: async (newBlog) => await addBlog(newBlog),
+    onSuccess: () => {
+      queryClient.invalidateQueries("blogs");
+      toast.success("Blog added successfully");
+    },
+    onError: (error) => {
+      console.log(error);
+      toast.error(error.message);
+    },
+  });
+
+  const {mutateAsync: deleteBlogFn} = useMutation({
+    mutationFn: async (id) => await deleteBlog(id),
+    onSuccess: () => {
+      queryClient.invalidateQueries("blogs");
+      toast.success("Blog deleted successfully");
+    },
+    onError: (error) => {
+      console.log(error);
+      toast.error(error.message);
+    },
+  });
+
+const {mutateAsync: PublishBlog} = useMutation({
+    mutationFn: async (id) => await updateBlogStatus(id, "published"),
+    onSuccess: () => {
+      queryClient.invalidateQueries("blogs");
+      toast.success("Blog published successfully");
+    },
+    onError: (error) => {
+      console.log(error);
+      toast.error(error.message);
+    },
+  });
+
+const {mutateAsync: UnpublishBlog} = useMutation({
+    mutationFn: async (id) => await  updateBlogStatus(id, "draft"),
+    onSuccess: () => {
+      queryClient.invalidateQueries("blogs");
+      toast.success("Blog unpublished successfully");
+    },
+    onError: (error) => {
+      console.log(error);
+      toast.error(error.message);
+    },
+  });
+
+
+  const {mutateAsync: updateBlogFn} = useMutation({
+    mutationFn: async ({id, newBlogInfo}) => await updateBlog(id, newBlogInfo),
+    onSuccess: () => {
+      queryClient.invalidateQueries("blogs");
+      toast.success("Blog updated successfully");
+    },
+    onError: (error) => {
+      console.log(error);
+      toast.error(error.message);
+    },
+  });
+
+
   return {
     blogs,
     isBlogsLoading,
-    // UpdateBlogStatus,
-    // deleteBlogFn,
-    // addBlogFn,
-    // updateBlogFn,
+    addBlogFn,
+    deleteBlogFn,
+    PublishBlog,
+    UnpublishBlog,
+    updateBlogFn
   };
 };
 
