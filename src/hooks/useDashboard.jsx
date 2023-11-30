30972c12d5f5fd3bfa1e2dc0c7b05cbc46be7b15
Mehedi Hasan <devbytemehedi@gmail.com>
2023-11-30 20:09:49 +0600

    Refactor useDashboard hook to include totalFunds

--- src/hooks/useDashboard.jsx
+++ src/hooks/useDashboard.jsx
@@ -3,33 +3,42 @@ import {getTotalRequests} from "../api/requests";
 import {getTotalUsers} from "../api/users";
 import useUser from "./useUser";
 import useAuth from "./useAuth";
+import axiosSecure from "../api";
 
 const useDashboard = () => {
   const {user, isUserLoading} = useAuth();
   const {currentUser} = useUser();
   const userMail = user?.email;
+  const isAuthorized =
+    currentUser?.role === "volunteer" || currentUser?.role === "admin";
   const {data: totalUsers, isLoading: isTotalUsersLoading} = useQuery({
-    enabled: !isUserLoading && !!userMail && currentUser?.role === "admin",
+    enabled: !isUserLoading && !!userMail && !!isAuthorized,
     queryKey: ["totalUsers"],
     queryFn: async () => await getTotalUsers(),
   });
 
   const {data: totalRequests, isLoading: isTotalRequestsLoading} = useQuery({
-    enabled: !isUserLoading && !!userMail && currentUser?.role === "admin",
+    enabled: !isUserLoading && !!userMail && !!isAuthorized,
     queryKey: ["totalRequests"],
     queryFn: async () => await getTotalRequests(),
   });
 
-  // const {data: totalFunds, isLoading: isTotalFundsLoading} = useQuery({
-  //   queryKey: ["totalFunds"],
-  //   queryFn: async () => await getTotalFunds(),
-  // });
+  const {data: totalFunds, isLoading: isTotalFundsLoading} = useQuery({
+    enabled: !isUserLoading && !!userMail && !!isAuthorized,
+    queryKey: ["totalFunds"],
+    queryFn: async () => {
+      const {data} = await axiosSecure("/funding-ammount");
+      return data[0];
+    },
+  });
 
   return {
     totalUsers,
     isTotalUsersLoading,
     totalRequests,
     isTotalRequestsLoading,
+    totalFunds,
+    isTotalFundsLoading,
   };
 };
 
